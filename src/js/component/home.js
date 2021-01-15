aaf996e775af03a7cc0fb62711208dc7ccf92849
Jose Hurtado <hello@josehurtado.net>
2021-01-15 23:49:57 +0000

    css edits

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,11 +4,13 @@ import React from "react";
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>TODO LIST</h1>
-			<br />
+			<div className="container form">
+				<h1>TODO LIST</h1>
+				<br />
 
-			<div className="todoList">
-				<input className="todoInput" placeholder="Add Task" />
+				<div className="todoList">
+					<input className="todoInput" placeholder="Add Task" />
+				</div>
 			</div>
 		</div>
 	);
