08e9b94f1753a0723b0563caa8ffdbb706abd89e
Jose Hurtado <hello@josehurtado.net>
2021-01-17 20:30:11 +0000

    CSS Updates

---
+++ src/js/component/todoForm.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+//TODOlist Function
+export function Home() {
+	const [theList, getList] = useState([
+		"Learn Javascript",
+		"Schedule Mentorships",
+		"Practice React"
+	]);
+	const [userInput, setUserInput] = useState([""]); // First empty userInput
+
+	const handleKeyUp = event => {
+		if (event.keyCode == 13 && userInput != "") {
+			getList(theList.concat(userInput));
+			setUserInput("");
+		}
+	};
+	// handleKeyUp from onKeyUp on input text with event passed as default
+	// check if event keycode is 13 (enter) and input is not blank to continue
+	// use state getList to add concat version of userInput into theList
+
+	const itemDelete = index => {
+		var updatedList = theList.filter(
+			(task, taskIndex) => index != taskIndex
+		);
+		getList(updatedList);
+	};
+	// create new variable with updated list > filter to check if index matches original index from list. then use getList to update to new list.
+
+	return (
+		<div className="container form text-center mt-5">
+			<h1>To-Do List</h1>
+			<br />
+
+			<div className="todoList">
+				<input
+					className="todoInput"
+					onChange={event => setUserInput(event.target.value)}
+					value={userInput}
+					onKeyUp={handleKeyUp}
+				/>
+				<br />
+				<div className="mt-3">
+					<ul className="list-group list-group-flush">
+						{theList.map((value, index) => {
+							return (
+								<li className="list-group-item" key={index}>
+									{value}
+									<button
+										type="button"
+										onClick={() => itemDelete(index)}
+										className="btn btn-primary">
+										X
+									</button>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+}
